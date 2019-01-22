354a9b4e46ccdd47fddfa9e9ad8b5a2ddf8c3402
Kirby <kedarst@cedarville.edu>
2019-01-22 14:44:39 -0500

    forgot to push

--- app/src/main/java/com/example/tourguidedrone/MainActivity.java
+++ app/src/main/java/com/example/tourguidedrone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tourguidedrone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner selectDestList = findViewById(R.id.selectDestList);
+        String[] destListString = new String[] {"Stevens Student Center", "Dixon Ministry Center",
+                "Center for Biblical and Theological Studies", "Engineering and Science Center",
+                "Health and Science Center"};
+        ArrayAdapter<String> selectDestListAdapter = new ArrayAdapter<String>(MainActivity.this
+                android.R.layout.simple_spinner_dropdown_item, destListString);
+
     }
 }
