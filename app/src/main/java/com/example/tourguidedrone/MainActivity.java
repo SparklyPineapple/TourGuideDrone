321963e5670d041cb3591859d7452b813840aed1
Kirby <kedarst@cedarville.edu>
2019-01-22 15:17:44 -0500

    destination drop down menu made + has stuff in it. UI configured

--- app/src/main/java/com/example/tourguidedrone/MainActivity.java
+++ app/src/main/java/com/example/tourguidedrone/MainActivity.java
@@ -12,12 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //set up the choices for the dropdown destination list
+
         Spinner selectDestList = findViewById(R.id.selectDestList);
         String[] destListString = new String[] {"Stevens Student Center", "Dixon Ministry Center",
                 "Center for Biblical and Theological Studies", "Engineering and Science Center",
                 "Health and Science Center"};
-        ArrayAdapter<String> selectDestListAdapter = new ArrayAdapter<String>(MainActivity.this
+        ArrayAdapter<String> selectDestListAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_dropdown_item, destListString);
+        selectDestList.setAdapter(selectDestListAdapter);
 
     }
 }
